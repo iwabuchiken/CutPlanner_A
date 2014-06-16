3bbe7eda5c798b58c07d25e85a665c73ca7fef83
iwabuchiken <iwabuchi.k.2010@gmail.com>
2014-06-17 00:19:29 +0900

    D-1(m)
    v-1.1
    ---
    1. MainActv, layout
    
    	==> done

---
+++ src/cp/main/MainActv.java
@@ -0,0 +1,212 @@
+package cp.main;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.ListActivity;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.os.Vibrator;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.nio.channels.FileChannel;
+
+//import app.main.R;
+
+
+public class MainActv extends Activity {
+	
+	public static Vibrator vib;
+
+	
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+    	/*----------------------------
+		 * 1. super
+		 * 2. Set content
+		 * 2-2. Set title
+		 * 3. Initialize => vib
+		 * 
+		 *  4. Set list
+		 *  5. Set listener => Image buttons
+		 *  6. Set path label
+		 *  
+		 *  7. Initialize preferences
+		 *  
+		 *  8. Refresh DB
+			----------------------------*/
+		
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.actv_main);
+        
+        /*----------------------------
+		 * 2-2. Set title
+			----------------------------*/
+		this.setTitle(this.getClass().getName());
+        
+        vib = (Vibrator) this.getSystemService(Context.VIBRATOR_SERVICE);
+
+        
+    }//public void onCreate(Bundle savedInstanceState)
+
+    @Override
+	protected void onDestroy() {
+		/*----------------------------
+		 * 1. Reconfirm if the user means to quit
+		 * 2. super
+		 * 3. Clear => prefs_main
+		 * 4. Clear => list_root_dir
+			----------------------------*/
+		// Log
+		Log.d("MainActv.java" + "["
+				+ Thread.currentThread().getStackTrace()[2].getLineNumber()
+				+ "]", "onDestroy()");
+		
+		super.onDestroy();
+		
+	}//protected void onDestroy()
+
+	@Override
+	public boolean onKeyDown(int keyCode, KeyEvent event) {
+		
+//		Methods.confirm_quit(this, keyCode);
+		
+		return super.onKeyDown(keyCode, event);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// 
+//		MenuInflater mi = getMenuInflater();
+//		mi.inflate(R.menu.menu_main, menu);
+//		
+		return super.onCreateOptionsMenu(menu);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+
+//		case R.id.opt_menu_main_db://----------------------------------
+//			
+//			Methods_dlg.dlg_Db_Activity(this);
+//			
+//			break;// case R.id.main_opt_menu_create_folder
+			
+		}//switch (item.getItemId())
+		
+		return super.onOptionsItemSelected(item);
+		
+	}//public boolean onOptionsItemSelected(MenuItem item)
+
+	@Override
+	protected void onPause() {
+		
+		super.onPause();
+
+		Log.d("MainActv.java" + "["
+				+ Thread.currentThread().getStackTrace()[2].getLineNumber()
+				+ "]", "onPause()");
+
+//		// Log
+//		Log.d("MainActv.java" + "["
+//				+ Thread.currentThread().getStackTrace()[2].getLineNumber()
+//				+ "]", "prefs_main: " + Methods.get_currentPath_from_prefs(this));
+		
+		
+	}
+
+	@Override
+	protected void onResume() {
+		/*********************************
+		 * 1. super
+		 * 2. Set enables
+		 *********************************/
+		super.onResume();
+
+		Log.d("MainActv.java" + "["
+				+ Thread.currentThread().getStackTrace()[2].getLineNumber()
+				+ "]", "onResume()");
+
+		
+		/*********************************
+		 * 2. Set enables
+		 *********************************/
+//		ImageButton ib_up = (ImageButton) findViewById(R.id.v1_bt_up);
+//		
+//		String curDirPath = Methods.get_currentPath_from_prefs(this);
+//		
+//		if (curDirPath.equals(dname_base)) {
+//			
+//			ib_up.setEnabled(false);
+//			
+//			ib_up.setImageResource(R.drawable.ifm8_up_disenabled);
+//			
+//		} else {//if (this.currentDirPath == this.dpath_base)
+//		
+//			ib_up.setEnabled(true);
+//
+//			
+//			ib_up.setImageResource(R.drawable.ifm8_up);
+//		
+//		}//if (this.currentDirPath == this.dpath_base)
+		
+	}//protected void onResume()
+
+	@Override
+	protected void onStart() {
+		/*----------------------------
+		 * 1. Refresh DB
+			----------------------------*/
+//		refresh_db();
+//		SharedPreferences prefs_main =
+//							this.getSharedPreferences(this.getString(R.string.prefs_shared_prefs_name), 0);
+//		
+////		// Log
+////		Log.d("MainActv.java" + "["
+////				+ Thread.currentThread().getStackTrace()[2].getLineNumber()
+////				+ "]", "prefs_main: db_refresh => " + prefs_main.getBoolean(this.getString(R.string.prefs_db_refresh_key), false));
+//		
+//		if(prefs_main.getBoolean(this.getString(R.string.prefs_db_refresh_key), false)) {
+//			
+//			Methods.start_refreshDB(this);
+//			
+//		} else {//if(prefs_main.getBoolean(this.getString(R.string.prefs_db_refresh_key), false))
+//			
+////			// Log
+////			Log.d("MainActv.java" + "["
+////					+ Thread.currentThread().getStackTrace()[2].getLineNumber()
+////					+ "]", "Prefs: db_refresh => " + false);
+//			
+//		}//if(prefs_main.getBoolean(this.getString(R.string.prefs_db_refresh_key), false))
+		
+		super.onStart();
+	}//protected void onStart()
+
+	
+
+}//public class MainActv extends Activity
